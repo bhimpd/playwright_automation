d09bc0f6531eebe29525534b8fc7b07341a94b13
bhimpd <dreamypd73@gmail.com>
2025-07-06 19:07:33 +0545

    reformat the code

--- utilis/userData.ts
+++ utilis/userData.ts
@@ -4,20 +4,26 @@ import { dirname } from "path";
 const filePath = "./tmp/credentials.txt";
 
 export function saveCredentials(email: string, password: string): void {
+
     const dir = dirname(filePath);
+    
     if (!existsSync(dir)) {
         mkdirSync(dir, { recursive: true });
     }
+
     const timestamp = new Date().toISOString();
     const data = { email, password, createdAt: timestamp };
+    
     writeFileSync(filePath, JSON.stringify(data, null, 2)); //  Save as JSON with formatting
 }
 
 export function getCredentials(): { email: string; password: string } {
+
     if (existsSync(filePath)) {
         const fileData = readFileSync(filePath, "utf-8").trim();
         return JSON.parse(fileData);
     } else {
         throw new Error("Credentials file does not exist. Run the registration test first.");
     }
+
 }
