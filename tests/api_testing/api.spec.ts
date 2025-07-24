95efc742c1335ce1c64a04543f7adc4dad8db2e2
bhimpd <dreamypd73@gmail.com>
2025-07-24 20:52:04 +0545

    assert the response in the apis

--- tests/api_testing/api.spec.ts
+++ tests/api_testing/api.spec.ts
@@ -5,11 +5,19 @@ test("API: GET-Request:: Fetch the products", async({request}) => {
     const baseUrl = process.env.API_BASEURL;
 
     const response = await request.get(`${baseUrl}/productsList`);
+    console.log("RESPONSE:::::",response);
+
     const data = await response.json();
     console.log("Response Data ::", JSON.stringify(data, null, 4));
 
     expect (response.status()).toBe(200);
+    expect(data).toHaveProperty('responseCode');
+    expect(data.responseCode).toBe(200);
+    expect(data).toHaveProperty('products');
+    expect(Array.isArray(data.products)).toBeTruthy();
+    const productLength = await data.products.length;
+    console.log("Product Length = ",productLength);
 
-
+    expect(data.products.length).toBeGreaterThan(0);
 
 });
